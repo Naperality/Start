8e52928d990dc7f0522a9eb60e847650fc4edc70
Naperality <napolytan@gmail.com>
2024-01-19 18:27:35 +0800

    90% Done

--- JAVASCRIPT/2nd-Exercise/script.js
+++ JAVASCRIPT/2nd-Exercise/script.js
@@ -46,6 +46,43 @@ function getCaloriesFromInputs(list){
         const invalidInputMatch = isInvalidInput(currVal);
         if (invalidInputMatch) {
             alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError=true;
+            return null;
         }
+        calories+= Number(currVal);
     }
+    return(calories);
+}
+
+function calculateCalories(e){
+ e.preventDefault();
+ isError = false;
+
+ const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+ const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+ const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');   
+ const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+ const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');   
+
+ const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+ const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+ const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+ const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+ const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
+ const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+ if(isError){
+    return null;
+ }
+ const consumedCalories = breakfastCalories+lunchCalories+dinnerCalories+snacksCalories;
+ const remainingCalories = budgetCalories-consumedCalories+exerciseCalories;
+ const surplusOrDeficit = remainingCalories>=0 ? "Surplus":"Deficit";
+
+ output.innerHTML = `
+ <span class = "${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+ <hr>
+ <p>${budgetCalories} Calories Budgeted</p>
+ `;
+
 }
