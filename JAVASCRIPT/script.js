a798b626bb54e98ae2b0c7394b78c641f09e921e
Naperality <napolytan@gmail.com>
2024-01-27 13:58:30 +0800

    80% Done

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,45 +1,78 @@
+//declaration
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
 
-//function for decimal to binary
+//variables for animation
+const animationData = [
+    {
+        inputVal: 5,
+        marginTop: 300,
+        addElDelay: 1000,
+    },
+    {
+        inputVal: 2,
+        marginTop: -200,
+        addElDelay: 1500,
+    },
+    {
+        inputVal: 1,
+        marginTop: -200,
+        addElDelay: 2000,
+    }
+];
+
+// //function for decimal to binary
+// const decimalToBinary = (input)=>{
+//     //declare
+//     const binary = "";
+//     //0 value to show 0
+//     if (input === 0) {
+//         binary = "0";
+//     }
+//     //loop for conversion
+//     while (input>0) {
+//         binary = (input % 2) + binary;
+//         input = Math.floor(input/2);
+//     }
+//     //displaying result to web
+//     result.innerText = binary;
+// };
+
+//recursive function for conversion
 const decimalToBinary = (input)=>{
-    //declare arrays 
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
-    //checking if value is 0 to convert ir to decimal
-    if(input === 0){
-        result.innerText = "0";
-        return;
+    if (input === 0||input === 1) {
+        return String(input);
     }
-    //while loop for putting inputs, quotient, and remainders
-    while (input>0) {
-        const quotient = Math.floor(input / 2);
-        const remainder = input % 2;
+    else{
+        return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+    }
+};
 
-        inputs.push(input);
-        quotients.push(quotient);
-        remainders.push(remainder);
-        input = quotient;
-    }
-    //cheking results
-    console.log("Inputs: ",inputs);
-    console.log("Quotients: ",quotients);
-    console.log("Remainders: ",remainders); //opposite values of binary
-    //correcting and showing on webpage
-    const binaryNumber = remainders.reverse().join("");
-    result.innerText = binaryNumber;
+//show animation
+const showAnimation = () =>{
+    result.innerText = "Call Stack Animation";
+
+    animationData.forEach(obj => {
+        setTimeout(()=>{},obj.addElDelay);
+    });
 };
 
 //function for user inputs (numbers, decimals)
 const checkUserInput = ()=>{
+    const inputInt = parseInt(numberInput.value);
     //this checks input value to be number integer only or empty
-    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    if (!numberInput.value || isNaN(inputInt)) {
         alert("Please provide a decimal number");
         return;
     }
-    decimalToBinary(parseInt(numberInput.value));
+    //check user input of 5 to show animation
+    if(inputInt === 5){
+        showAnimation();
+        return;
+    }
+    result.textContent =  decimalToBinary(inputInt);
     //deleting the value of previous number
     numberInput.value = "";
 };
