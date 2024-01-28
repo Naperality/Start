9c8fcf01368af1e3d85f2a50069cbfcef6058c02
Naperality <napolytan@gmail.com>
2024-01-28 11:44:53 +0800

    Done 100%

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -3,20 +3,28 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 const animationContainer = document.getElementById("animation-container");
-
 //variables for animation
 const animationData = [
     {
+        msg:'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+        showMsgDelay: 15000,
+        removeElDelay:20000,
         inputVal: 5,
         marginTop: 300,
         addElDelay: 1000,
     },
     {
+        msg:'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+        showMsgDelay: 10000,
+        removeElDelay:15000,
         inputVal: 2,
         marginTop: -200,
         addElDelay: 1500,
     },
     {
+        msg:'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.',
+        showMsgDelay: 5000,
+        removeElDelay:10000,
         inputVal: 1,
         marginTop: -200,
         addElDelay: 2000,
@@ -53,10 +61,29 @@ const decimalToBinary = (input)=>{
 //show animation
 const showAnimation = () =>{
     result.innerText = "Call Stack Animation";
-
+    //array reading of animation data or delays
     animationData.forEach(obj => {
-        setTimeout(()=>{},obj.addElDelay);
+        //using the data to show in html
+        setTimeout(()=>{
+            animationContainer.innerHTML += `
+                <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px" class="animation-frame">
+                    decimalToBinary(${obj.inputVal})
+                </p>
+                `;
+        },obj.addElDelay);
+        //paragraph element 
+        setTimeout(()=>{
+            document.getElementById(obj.inputVal).textContent = obj.msg;
+        },obj.showMsgDelay);
+        //removing elements
+        setTimeout(()=>{
+            document.getElementById(obj.inputVal).remove();
+        },obj.removeElDelay);
     });
+    //show the result of conversion on num 5
+    setTimeout(()=>{
+        result.textContent = decimalToBinary(5);
+    },20000);
 };
 
 //function for user inputs (numbers, decimals)
