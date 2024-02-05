ebce34e9fc74271c0201125687c1ebdda7f38062
Naperality <napolytan@gmail.com>
2024-02-05 18:51:36 +0800

    Done Project 3

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -34,13 +34,32 @@ const median = (nums) => {
 };
 
 //applying function 
-const applyFunction = (str) =>{};
+const applyFunction = (str) => {
+  const noHigh = highPrecedence(str);
+  const infix = /([\d.]+)([+-])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = args => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn,args): match );
+};
 
 //object to facilitates functions
 const spreadsheetFunctions  = {
-    sum,
-    average,
-    median,
+  "": (arg) => arg,
+  sum,
+  average,
+  median,
+  even: nums => nums.filter(isEven),
+  someeven: nums => nums.some(isEven),
+  everyeven: nums => nums.every(isEven),
+  firsttwo: nums => nums.slice(0, 2),
+  lasttwo: nums => nums.slice(-2),
+  has2: nums => nums.includes(2),
+  increment: nums => nums.map(num => num + 1),
+  random: ([x, y]) => Math.floor(Math.random() * y + x),
+  range: nums => range(...nums),
+  nodupes: nums => [...new Set(nums).values()]
 }; 
 
 //declare range array using funciton
@@ -59,6 +78,8 @@ const evalFormula = (x, cells) => {
   const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
   const cellRegex = /[A-J][1-9][0-9]?/gi;
   const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
+  const functionExpanded = applyFunction(cellExpanded);
+  return functionExpanded===x ? functionExpanded:evalFormula(functionExpanded,cells);
 }
 
 //windows upload 
@@ -89,7 +110,7 @@ window.onload = () => {
 const update = (event) => {
     const element = event.target;
     const value = element.value.replace(/\s/g, "");
-    if (!value.includes(element.id)&&value.charAt(0)=== "=") {
-
+    if (!value.includes(element.id) && value.startsWith('=')) {
+      element.value = evalFormula(value.slice(1), Array.from(document.getElementById("container").children));
     }
 };
