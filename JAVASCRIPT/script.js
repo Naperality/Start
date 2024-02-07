44ee20a0f9c51fcd4e68a1c054cf85f75cc18521
Naperality <napolytan@gmail.com>
2024-02-07 22:29:13 +0800

    15% Done

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -3,12 +3,19 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton= document.getElementById("check-message-btn");
 
+//regex to catch
+const helpRegex = /please help/; 
+
 //event listener
 checkMessageButton.addEventListener("click",()=>{
     if (messageInput.value === "") {
         alert("Please enter a message.");
         return;
     }
+    result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
 });
 
 //is spam function
