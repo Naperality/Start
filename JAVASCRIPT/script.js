92934f3047328054b7dcc21ee76221bd2de56e11
Naperality <napolytan@gmail.com>
2024-02-08 22:12:37 +0800

    20% done

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -4,19 +4,25 @@ const result = document.getElementById("result");
 const checkMessageButton= document.getElementById("check-message-btn");
 
 //regex to catch
-const helpRegex = /please help/; 
+const helpRegex = /please help|assist me/i;
+
+//arrays of  regex
+const denyList = [helpRegex];
+
+//is spam function
+const isSpam = (msg) => helpRegex.test(msg);
 
 //event listener
-checkMessageButton.addEventListener("click",()=>{
-    if (messageInput.value === "") {
-        alert("Please enter a message.");
-        return;
-    }
-    result.textContent = isSpam(messageInput.value)
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.value === "") {
+    alert("Please enter a message.");
+    return;
+  }
+
+  result.textContent = isSpam(messageInput.value)
     ? "Oh no! This looks like a spam message."
     : "This message does not seem to contain any spam.";
   messageInput.value = "";
 });
 
-//is spam function
-const isSpam = (msg) => false;
+
