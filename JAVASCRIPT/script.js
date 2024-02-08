d5e9fb1f74ff717403eed39f5d36032b8361d684
Naperality <napolytan@gmail.com>
2024-02-08 23:03:55 +0800

    Done

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -10,28 +10,26 @@ const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
 //strings- "[e3]" means e and 3 to match, "\s" looks for spaces or line breaks, 
 //"^" means beginning of string, "$" means end of string
-const freeRegex = /(?:\s|^)fr[e3][e3] m[o0]n[e3]y(?:\s|$)/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 //stock alert 
 const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
 //dear friend
-const dearRegex = /dear friend/i;
+const dearRegex = /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:\s|$)/i;
 
 //arrays of  regex
-const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex,dearRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
 //is spam function
-const isSpam = (msg) => helpRegex.test(msg);
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
 //event listener
-checkMessageButton.addEventListener("click", () => {
+ccheckMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
     alert("Please enter a message.");
     return;
   }
 
-  result.textContent = isSpam(messageInput.value)
-    ? "Oh no! This looks like a spam message."
-    : "This message does not seem to contain any spam.";
+  result.textContent = isSpam(messageInput.value) ? "Oh no! This looks like a spam message.": "This message does not seem to contain any spam.";
   messageInput.value = "";
 });
 
