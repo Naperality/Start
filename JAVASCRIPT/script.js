d8a9e33bc9e5bb24ebd4000db9c811305a28b7e9
Naperality <napolytan@gmail.com>
2024-02-08 22:37:39 +0800

    80% Done

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -5,11 +5,16 @@ const checkMessageButton= document.getElementById("check-message-btn");
 
 //regex to catch
 const helpRegex = /please help|assist me/i;
-//dollar 
-const dollarRegex = /[0-9]+ hundred|thousand|million|billion dollars/i;
+//dollar - "()" capture , "|" or, "[]" get all, "?" optional
+//"?:" non capturing group but can still match 
+const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+//strings- "[e3]" means e and 3 to match, "\s" looks for spaces or line breaks, 
+//"^" means beginning of string, "$" means end of string
+const freeRegex = /(?:\s|^)fr[e3][e3] m[o0]n[e3]y(?:\s|$)/i;
+
 
 //arrays of  regex
-const denyList = [helpRegex,dollarRegex];
+const denyList = [helpRegex,dollarRegex,freeRegex];
 
 //is spam function
 const isSpam = (msg) => helpRegex.test(msg);
