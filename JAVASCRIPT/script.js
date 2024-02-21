a334cc972ae542b7e9e613c29602132f3a7595cd
Naperality <napolytan@gmail.com>
2024-02-21 21:26:26 +0800

    Start

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -2,18 +2,3 @@ const cartContainer = document.getElementById("cart-container");
 const productsContainer = document.getElementById("products-container");
 const dessertCards = document.getElementById("dessert-card-container");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
